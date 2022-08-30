3bf5f10bd58adb382986a9ebeba4bec1579954a4
Nick De Breuck <nick.debreuck@outlook.com>
2022-08-31 00:54:52 +0100

    added codecvt header

---
+++ source/include/0_extern/rex_std/codecvt.h
@@ -0,0 +1,15 @@
+// ============================================
+//
+// REX - STANDARD LIBRARY IMPLEMENTATION
+//
+// Author: Nick De Breuck
+// Twitter: @nick_debreuck
+// 
+// File: condecvt.h
+// Copyright (c) Nick De Breuck 2022
+//
+// ============================================
+
+#pragma once
+
+static_assert(false, "this file is deprecated from C++17")
