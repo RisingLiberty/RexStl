a410b27cd1a2c9ca95f4b5f66b4b8c5b9ddff584
Nick De Breuck <nick.debreuck@outlook.com>
2022-08-31 00:49:42 +0100

    added bit header

---
+++ source/include/0_extern/rex_std/bit.h
@@ -0,0 +1,43 @@
+// ============================================
+//
+// REX - STANDARD LIBRARY IMPLEMENTATION
+//
+// Author: Nick De Breuck
+// Twitter: @nick_debreuck
+// 
+// File: bit.h
+// Copyright (c) Nick De Breuck 2022
+//
+// ============================================
+
+#pragma once
+
+#include "rex_std/std_alias_defines.h"
+#include "rex_std/disable_std_checking.h"
+
+#include <bit>
+
+namespace rsl
+{
+    REX_STD_CLASS_ALIAS(endian);
+
+    REX_STD_FUNC_ALIAS(bit_cast);
+    REX_STD_FUNC_ALIAS(has_single_bit);
+    REX_STD_FUNC_ALIAS(bit_ceil);
+    REX_STD_FUNC_ALIAS(bit_floor);
+    REX_STD_FUNC_ALIAS(bit_width);
+    REX_STD_FUNC_ALIAS(rotl);
+    REX_STD_FUNC_ALIAS(rotr);
+    REX_STD_FUNC_ALIAS(countl_zero);
+    REX_STD_FUNC_ALIAS(countl_one);
+    REX_STD_FUNC_ALIAS(countr_zero);
+    REX_STD_FUNC_ALIAS(countr_one);
+    REX_STD_FUNC_ALIAS(popcount);
+
+#ifdef REX_ENABLE_WITH_CPP23
+    REX_STD_FUNC_ALIAS(byteswap);
+#endif // REX_ENABLE_WITH_CPP23
+
+}
+
+#include "rex_std/enable_std_checking.h"
