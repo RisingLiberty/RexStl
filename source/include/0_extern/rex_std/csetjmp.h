13a453e1f5c654b8085dff0cdbdbc1cd5e0e146b
Nick De Breuck <nick.debreuck@outlook.com>
2022-08-31 00:56:02 +0100

    added csetjmp header

---
+++ source/include/0_extern/rex_std/csetjmp.h
@@ -0,0 +1,21 @@
+// ============================================
+//
+// REX - STANDARD LIBRARY IMPLEMENTATION
+//
+// Author: Nick De Breuck
+// Twitter: @nick_debreuck
+// 
+// File: csetjmp.h
+// Copyright (c) Nick De Breuck 2022
+//
+// ============================================
+
+#pragma once
+
+// usage of setjmp is highly discouraged, even by Microsoft
+// "Because setjmp and longjmp don't support correct destruction of stack frame objects portably between C++ compilers, 
+// and because they might degrade performance by preventing optimization on local variables, we don't recommend their use in C++ programs. We recommend you use try and catch constructs instead."
+// They're basically fancy goto statements in C++.
+// goto statement should never be used in C++, therefore we don't implement setjmp in rsl.
+
+static_assert(false, "using csetjmp in rsl is not allowed");
