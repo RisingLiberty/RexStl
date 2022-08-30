2603bb3dbad4b25b51eb0f22439dc4387b569728
Nick De Breuck <nick.debreuck@outlook.com>
2022-08-31 00:49:34 +0100

    added barrier header

---
+++ source/include/0_extern/rex_std/barrier.h
@@ -0,0 +1,30 @@
+// ============================================
+//
+// REX - STANDARD LIBRARY IMPLEMENTATION
+//
+// Author: Nick De Breuck
+// Twitter: @nick_debreuck
+// 
+// File: barrier.h
+// Copyright (c) Nick De Breuck 2022
+//
+// ============================================
+
+#pragma once
+
+#ifdef REX_ENABLE_WITH_CPP20
+
+#include "rex_std/std_alias_defines.h"
+#include "rex_std/disable_std_checking.h"
+
+#include <barrier>
+
+namespace rsl
+{
+    template <typename CompletionFunction>
+    REX_STD_TEMPLATED_CLASS_ALIAS(barrier, CompletionFunction);
+}
+
+#include "rex_std/enable_std_checking.h"
+
+#endif // REX_ENABLE_WITH_CPP20
