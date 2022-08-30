7dd71108fb1af0a999ad843af5f8716cd31a0834
Nick De Breuck <nick.debreuck@outlook.com>
2022-08-31 00:52:45 +0100

    added cerrno header

---
+++ source/include/0_extern/rex_std/cerrno.h
@@ -0,0 +1,16 @@
+// ============================================
+//
+// REX - STANDARD LIBRARY IMPLEMENTATION
+//
+// Author: Nick De Breuck
+// Twitter: @nick_debreuck
+// 
+// File: cerrno.h
+// Copyright (c) Nick De Breuck 2022
+//
+// ============================================
+
+#pragma once
+
+#include "rex_std/enable_std_checking.h"
+#include <cerrno>
