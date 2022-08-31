6d54b0602a856a96c73abb6bea9205e1640079a8
Nick De Breuck <nick.debreuck@outlook.com>
2022-08-31 01:01:04 +0100

    added header to enable/disable usecase of std namespace

---
+++ source/include/0_extern/rex_std/disable_std_checking.h
@@ -0,0 +1,17 @@
+// ============================================ 
+//
+// REX - STANDARD LIBRARY IMPLEMENTATION
+//
+// Author: Nick De Breuck
+// Twitter: @nick_debreuck
+// 
+// File: disable_std_checking.h
+// Copyright (c) Nick De Breuck 2022
+//
+// ============================================
+
+// pragma once omitted on purpose
+
+#ifdef std
+#undef std
+#endif
---
+++ source/include/0_extern/rex_std/enable_std_checking.h
@@ -0,0 +1,23 @@
+// ============================================
+//
+// REX - STANDARD LIBRARY IMPLEMENTATION
+//
+// Author: Nick De Breuck
+// Twitter: @nick_debreuck
+// 
+// File: std_checking.h
+// Copyright (c) Nick De Breuck 2022
+//
+// ============================================
+
+// pragma once omitted on purpose
+
+#ifndef ENABLE_STD_CHECKING
+//#define ENABLE_STD_CHECKING
+#endif
+
+#ifdef ENABLE_STD_CHECKING
+#ifndef std
+#define std STATIC_WARNING("using std in codebase") std
+#endif
+#endif
