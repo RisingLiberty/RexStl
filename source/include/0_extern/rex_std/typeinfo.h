07fbeccc735b512e258a35a8db42fe900caadb7a
Nick De Breuck <nick.debreuck@outlook.com>
2022-08-31 01:12:43 +0100

    added typeinfo header

---
+++ source/include/0_extern/rex_std/typeinfo.h
@@ -0,0 +1,25 @@
+// ============================================
+//
+// REX - STANDARD LIBRARY IMPLEMENTATION
+//
+// Author: Nick De Breuck
+// Twitter: @nick_debreuck
+// 
+// File: typeinfo.h
+// Copyright (c) Nick De Breuck 2022
+//
+// ============================================
+
+#pragma once
+
+#include "rex_std/std_alias_defines.h"
+#include "rex_std/disable_std_checking.h"
+
+#include <typeinfo>
+
+namespace rsl
+{
+    REX_STD_CLASS_ALIAS(type_info);
+}
+
+#include "rex_std/enable_std_checking.h"
