69af0196790a7f859ec0aca1198a881ce595dca9
Nick De Breuck <nick.debreuck@outlook.com>
2022-08-31 01:00:09 +0100

    added cuchar header

---
+++ source/include/0_extern/rex_std/cuchar.h
@@ -0,0 +1,36 @@
+// ============================================
+//
+// REX - STANDARD LIBRARY IMPLEMENTATION
+//
+// Author: Nick De Breuck
+// Twitter: @nick_debreuck
+// 
+// File: cuchar.h
+// Copyright (c) Nick De Breuck 2022
+//
+// ============================================
+
+#pragma once
+
+#include "rex_std/std_alias_defines.h"
+#include "rex_std/disable_std_checking.h"
+
+#include <cuchar>
+
+namespace rsl
+{
+    REX_STD_CLASS_ALIAS(mbstate_t);
+    REX_STD_CLASS_ALIAS(size_t);
+
+    REX_STD_FUNC_ALIAS(mbrtoc16);
+    REX_STD_FUNC_ALIAS(c16rtomb);
+    REX_STD_FUNC_ALIAS(mbrtoc32);
+    REX_STD_FUNC_ALIAS(c32rtomb);
+
+#ifdef REX_ENABLE_WITH_CPP20
+    REX_STD_FUNC_ALIAS(mbrtoc8);
+    REX_STD_FUNC_ALIAS(c8rtomb);
+#endif // REX_ENABLE_WITH_CPP20
+}
+
+#include "rex_std/enable_std_checking.h"
