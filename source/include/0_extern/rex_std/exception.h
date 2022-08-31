7cb75bb6193d5f02c2b53123053596c5eeb4323f
Nick De Breuck <nick.debreuck@outlook.com>
2022-08-31 01:01:14 +0100

    added exception header

---
+++ source/include/0_extern/rex_std/exception.h
@@ -0,0 +1,18 @@
+// ============================================
+//
+// REX - STANDARD LIBRARY IMPLEMENTATION
+//
+// Author: Nick De Breuck
+// Twitter: @nick_debreuck
+// 
+// File: exception.h
+// Copyright (c) Nick De Breuck 2022
+//
+// ============================================
+
+#pragma once
+
+static_assert(false, "exceptions are disabled in rex standard");
+
+#include "rex_std/enable_std_checking.h"
+#include <exception>
