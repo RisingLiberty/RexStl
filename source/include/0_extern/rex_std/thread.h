6ec62a827fb73cbe627f018c2e852a7293fe5d6c
Nick De Breuck <nick.debreuck@outlook.com>
2022-08-31 01:11:50 +0100

    added thread header

---
+++ source/include/0_extern/rex_std/thread.h
@@ -0,0 +1,43 @@
+// ============================================
+//
+// REX - STANDARD LIBRARY IMPLEMENTATION
+//
+// Author: Nick De Breuck
+// Twitter: @nick_debreuck
+// 
+// File: thread.h
+// Copyright (c) Nick De Breuck 2022
+//
+// ============================================
+
+#pragma once
+
+#include "rex_std/std_alias_defines.h"
+#include "rex_std/disable_std_checking.h"
+
+#include <thread>
+
+namespace rsl
+{
+    namespace this_thread
+    {
+        using namespace std::this_thread;
+    }
+
+    REX_STD_CLASS_ALIAS(thread);
+    REX_STD_CLASS_ALIAS(jthread);
+
+    //REX_STD_FUNC_ALIAS(swap); // Doesn't work because rsl has its own swap
+
+    REX_STD_FUNC_ALIAS(operator==);
+    REX_STD_FUNC_ALIAS(operator!=);
+    REX_STD_FUNC_ALIAS(operator<);
+    REX_STD_FUNC_ALIAS(operator<=);
+    REX_STD_FUNC_ALIAS(operator>);
+    REX_STD_FUNC_ALIAS(operator>=);
+
+    REX_STD_FUNC_ALIAS(operator<<);
+
+}
+
+#include "rex_std/enable_std_checking.h"
