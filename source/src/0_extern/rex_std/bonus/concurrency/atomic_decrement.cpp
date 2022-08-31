89ccb13c03af5bd1688e55f9d61cda4901486f64
Nick De Breuck <nick.debreuck@outlook.com>
2022-08-31 01:16:09 +0100

    added all bonus sources

---
+++ source/src/0_extern/rex_std/bonus/concurrency/atomic_decrement.cpp
@@ -0,0 +1,80 @@
+// ============================================ 
+//
+// REX - STANDARD LIBRARY IMPLEMENTATION
+//
+// Author: Nick De Breuck
+// Twitter: @nick_debreuck
+// 
+// File: atomic_decrement.cpp
+// Copyright (c) Nick De Breuck 2022
+//
+// ============================================
+
+#include "rex_std/bonus/concurrency/atomic_decrement.h"
+
+#include <Windows.h>
+
+/// [06/Aug/2022] RSL Comment: We wrap the functions in internal calls to avoid C28113 on MSVC
+namespace rsl::internal
+{
+    int16 atomic_decrement(int16* data)
+    {
+        return _InterlockedDecrement16(data);
+    }
+    int32 atomic_decrement(long* data)
+    {
+        return _InterlockedDecrement(data);
+    }
+    int64 atomic_decrement(int64* data)
+    {
+        return _InterlockedDecrement64(data);
+    }
+    uint16 atomic_decrement(uint16* data)
+    {
+        return static_cast<uint16>(atomic_decrement(reinterpret_cast<int16*>(data)));
+    }
+    uint32 atomic_decrement(ulong* data)
+    {
+        return _InterlockedDecrement(data);
+    }
+    uint64 atomic_decrement(uint64* data)
+    {
+        return static_cast<uint64>(atomic_decrement(reinterpret_cast<int64*>(data)));
+    }
+}
+
+int8 rsl::atomic_decrement(int8& val)
+{
+    long i32_val = val;
+    return static_cast<int8>(internal::atomic_decrement(&i32_val));
+}
+int16 rsl::atomic_decrement(int16& val)
+{
+    return internal::atomic_decrement(&val);
+}
+int32 rsl::atomic_decrement(int32& val)
+{
+    return internal::atomic_decrement(reinterpret_cast<long*>(&val));
+}
+int64 rsl::atomic_decrement(int64& val)
+{
+    return internal::atomic_decrement(&val);
+}
+
+uint8 rsl::atomic_decrement(uint8& val)
+{
+    ulong u32_val = val;
+    return static_cast<uint8>(internal::atomic_decrement(&u32_val));
+}
+uint16 rsl::atomic_decrement(uint16& val)
+{
+    return internal::atomic_decrement(&val);
+}
+uint32 rsl::atomic_decrement(uint32& val)
+{
+    return internal::atomic_decrement(reinterpret_cast<ulong*>(&val));
+}
+uint64 rsl::atomic_decrement(uint64& val)
+{
+    return internal::atomic_decrement(&val);
+}
---
+++ source/src/0_extern/rex_std/bonus/concurrency/atomic_increment.cpp
@@ -0,0 +1,80 @@
+// ============================================ 
+//
+// REX - STANDARD LIBRARY IMPLEMENTATION
+//
+// Author: Nick De Breuck
+// Twitter: @nick_debreuck
+// 
+// File: atomic_increment.cpp
+// Copyright (c) Nick De Breuck 2022
+//
+// ============================================
+
+#include "rex_std/bonus/concurrency/atomic_increment.h"
+
+#include <Windows.h>
+
+/// [06/Aug/2022] RSL Comment: We wrap the functions in internal calls to avoid C28113 on MSVC
+namespace rsl::internal
+{
+    int16 atomic_increment(int16* data)
+    {
+        return _InterlockedIncrement16(data);
+    }
+    int32 atomic_increment(long* data)
+    {
+        return _InterlockedIncrement(data);
+    }
+    int64 atomic_increment(int64* data)
+    {
+        return _InterlockedIncrement64(data);
+    }
+    uint16 atomic_increment(uint16* data)
+    {
+        return static_cast<uint16>(atomic_increment(reinterpret_cast<int16*>(data)));
+    }
+    uint32 atomic_increment(ulong* data)
+    {
+        return _InterlockedIncrement(data);
+    }
+    uint64 atomic_increment(uint64* data)
+    {
+        return static_cast<uint64>(atomic_increment(reinterpret_cast<int64*>(data)));
+    }
+}
+
+int8 rsl::atomic_increment(int8& val)
+{
+    long i32_val = val;
+    return static_cast<int8>(internal::atomic_increment(&i32_val));
+}
+int16 rsl::atomic_increment(int16& val)
+{
+    return internal::atomic_increment(&val);
+}
+int32 rsl::atomic_increment(int32& val)
+{
+    return internal::atomic_increment(reinterpret_cast<long*>(&val));
+}
+int64 rsl::atomic_increment(int64& val)
+{
+    return internal::atomic_increment(&val);
+}
+
+uint8 rsl::atomic_increment(uint8& val)
+{
+    ulong u32_val = val;
+    return static_cast<uint8>(internal::atomic_increment(&u32_val));
+}
+uint16 rsl::atomic_increment(uint16& val)
+{
+    return internal::atomic_increment(&val);
+}
+uint32 rsl::atomic_increment(uint32& val)
+{
+    return internal::atomic_increment(reinterpret_cast<ulong*>(&val));
+}
+uint64 rsl::atomic_increment(uint64& val)
+{
+    return internal::atomic_increment(&val);
+}
