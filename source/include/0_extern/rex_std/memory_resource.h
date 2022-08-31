4a5242321a3c616a6c5eab0e2035985ffce0ba35
Nick De Breuck <nick.debreuck@outlook.com>
2022-08-31 01:06:34 +0100

    added memory_resource header

---
+++ source/include/0_extern/rex_std/memory_resource.h
@@ -0,0 +1,28 @@
+// ============================================
+//
+// REX - STANDARD LIBRARY IMPLEMENTATION
+//
+// Author: Nick De Breuck
+// Twitter: @nick_debreuck
+// 
+// File: memory_resource.h
+// Copyright (c) Nick De Breuck 2022
+//
+// ============================================
+
+#pragma once
+
+#include "rex_std/std_alias_defines.h"
+#include "rex_std/enable_std_checking.h"
+
+#include <memory_resource>
+
+namespace rsl
+{
+    namespace pmr
+    {
+        using namespace std::pmr;
+    }
+}
+
+#include "rex_std/enable_std_checking.h"
