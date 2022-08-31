046465554688e486d1618ad4a979e994ff6b03b1
Nick De Breuck <nick.debreuck@outlook.com>
2022-08-31 01:11:36 +0100

    added system_error header

---
+++ source/include/0_extern/rex_std/system_error.h
@@ -0,0 +1,50 @@
+// ============================================
+//
+// REX - STANDARD LIBRARY IMPLEMENTATION
+//
+// Author: Nick De Breuck
+// Twitter: @nick_debreuck
+// 
+// File: system_error.h
+// Copyright (c) Nick De Breuck 2022
+//
+// ============================================
+
+#pragma once
+
+#include "rex_std/std_alias_defines.h"
+#include "rex_std/disable_std_checking.h"
+
+#include <system_error>
+
+#include "rex_std/compare.h"
+
+namespace rsl
+{
+    REX_STD_CLASS_ALIAS(error_category);
+    REX_STD_CLASS_ALIAS(error_condition);
+    REX_STD_CLASS_ALIAS(errc);
+    REX_STD_CLASS_ALIAS(error_code);
+    REX_STD_CLASS_ALIAS(system_error);
+
+    template <typename T>
+    REX_STD_TEMPLATED_CLASS_ALIAS(is_error_code_enum, T);
+    template <typename T>
+    REX_STD_TEMPLATED_CLASS_ALIAS(is_error_condition_enum, T);
+
+    template <typename T>
+    REX_STD_TEMPLATED_OBJECT_ALIAS(is_error_code_enum_v, T);
+    template <typename T>
+    REX_STD_TEMPLATED_OBJECT_ALIAS(is_error_condition_enum_v, T);
+
+    REX_STD_FUNC_ALIAS(generic_category);
+    REX_STD_FUNC_ALIAS(system_category);
+    REX_STD_FUNC_ALIAS(operator==);
+    REX_STD_FUNC_ALIAS(operator!=);
+    REX_STD_FUNC_ALIAS(operator<);
+    REX_STD_FUNC_ALIAS(operator<<);
+    REX_STD_FUNC_ALIAS(make_error_code);
+    REX_STD_FUNC_ALIAS(make_error_condition);
+}
+
+#include "rex_std/enable_std_checking.h"
