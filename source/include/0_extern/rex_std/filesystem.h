3e2330996028147f6646aa33ad399ee8df15a43f
Nick De Breuck <nick.debreuck@outlook.com>
2022-08-31 01:02:11 +0100

    added filesystem header

---
+++ source/include/0_extern/rex_std/filesystem.h
@@ -0,0 +1,28 @@
+// ============================================
+//
+// REX - STANDARD LIBRARY IMPLEMENTATION
+//
+// Author: Nick De Breuck
+// Twitter: @nick_debreuck
+// 
+// File: filesystem.h
+// Copyright (c) Nick De Breuck 2022
+//
+// ============================================
+
+#pragma once
+
+#include "rex_std/std_alias_defines.h"
+#include "rex_std/disable_std_checking.h"
+
+#include <filesystem>
+
+namespace rsl
+{
+    namespace filesystem
+    {
+        using namespace std::filesystem;
+    }
+}
+
+#include "rex_std/enable_std_checking.h"
