e2d8a32742f1506ee3e985d1d817607a5f6b363b
Nick De Breuck <nick.debreuck@outlook.com>
2022-08-31 01:06:49 +0100

    added new header

---
+++ source/include/0_extern/rex_std/new.h
@@ -0,0 +1,38 @@
+// ============================================
+//
+// REX - STANDARD LIBRARY IMPLEMENTATION
+//
+// Author: Nick De Breuck
+// Twitter: @nick_debreuck
+// 
+// File: new.h
+// Copyright (c) Nick De Breuck 2022
+//
+// ============================================
+
+#pragma once
+
+#include "rex_std/std_alias_defines.h"
+#include "rex_std/disable_std_checking.h"
+
+#include <new>
+
+namespace rsl
+{
+    REX_STD_CLASS_ALIAS(nothrow_t);
+    REX_STD_CLASS_ALIAS(align_val_t);
+    REX_STD_CLASS_ALIAS(destroying_delete_t);
+
+    REX_STD_CLASS_ALIAS(new_handler);
+
+    REX_STD_OBJECT_ALIAS(nothrow);
+    REX_STD_OBJECT_ALIAS(destroying_delete);
+    REX_STD_OBJECT_ALIAS(hardware_destructive_interference_size);
+    REX_STD_OBJECT_ALIAS(hardware_constructive_interference_size);
+
+    REX_STD_FUNC_ALIAS(get_new_handler);
+    REX_STD_FUNC_ALIAS(set_new_handler);
+    REX_STD_FUNC_ALIAS(launder);
+}
+
+#include "rex_std/enable_std_checking.h"
