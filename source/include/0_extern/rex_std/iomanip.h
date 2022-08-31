3a4610dac7bfef78bdc240287b0ec1d880347c37
Nick De Breuck <nick.debreuck@outlook.com>
2022-08-31 01:04:42 +0100

    added iomanip header

---
+++ source/include/0_extern/rex_std/iomanip.h
@@ -0,0 +1,35 @@
+// ============================================
+//
+// REX - STANDARD LIBRARY IMPLEMENTATION
+//
+// Author: Nick De Breuck
+// Twitter: @nick_debreuck
+// 
+// File: iomanip.h
+// Copyright (c) Nick De Breuck 2022
+//
+// ============================================
+
+#pragma once
+
+#include "rex_std/std_alias_defines.h"
+#include "rex_std/disable_std_checking.h"
+
+#include <iomanip>
+
+namespace rsl
+{
+    REX_STD_FUNC_ALIAS(resetiosflags);
+    REX_STD_FUNC_ALIAS(setiosflags);
+    REX_STD_FUNC_ALIAS(setbase);
+    REX_STD_FUNC_ALIAS(setfill);
+    REX_STD_FUNC_ALIAS(setprecision);
+    REX_STD_FUNC_ALIAS(setw);
+    REX_STD_FUNC_ALIAS(get_money);
+    REX_STD_FUNC_ALIAS(put_money);
+    REX_STD_FUNC_ALIAS(get_time);
+    REX_STD_FUNC_ALIAS(put_time);
+    REX_STD_FUNC_ALIAS(quoted);
+}
+
+#include "rex_std/enable_std_checking.h"
