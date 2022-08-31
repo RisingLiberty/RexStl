d998fdefb551bff054dbaf899a2b106c8dbabe8e
Nick De Breuck <nick.debreuck@outlook.com>
2022-08-31 01:02:33 +0100

    added flat_set header

---
+++ source/include/0_extern/rex_std/flat_set.h
@@ -0,0 +1,32 @@
+// ============================================ 
+//
+// REX - STANDARD LIBRARY IMPLEMENTATION
+//
+// Author: Nick De Breuck
+// Twitter: @nick_debreuck
+// 
+// File: flat_set.h
+// Copyright (c) Nick De Breuck 2022
+//
+// ============================================
+
+#pragma once
+
+#ifdef REX_ENABLE_WITH_CPP23
+
+#include "rex_std/compare.h"
+#include "rex_std/initializer_list.h"
+
+#include "rex_std/std_alias_defines.h"
+#include "rex_std/disable_std_checking.h"
+
+#include <flat_set>
+
+namespace rsl
+{
+    // No pages about this exist yet on cppreference
+}
+
+#include <rex_std/enable_std_checking.h>
+
+#endif // REX_ENABLE_WITH_CPP23
