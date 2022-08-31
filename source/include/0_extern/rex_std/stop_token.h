d7d9b934ae0d3d10e13c3be0a198d200165e11b6
Nick De Breuck <nick.debreuck@outlook.com>
2022-08-31 01:10:17 +0100

    added stop_token header

---
+++ source/include/0_extern/rex_std/stop_token.h
@@ -0,0 +1,37 @@
+// ============================================
+//
+// REX - STANDARD LIBRARY IMPLEMENTATION
+//
+// Author: Nick De Breuck
+// Twitter: @nick_debreuck
+// 
+// File: stop_token.h
+// Copyright (c) Nick De Breuck 2022
+//
+// ============================================
+
+#pragma once
+
+#ifdef REX_ENABLE_WITH_CPP20
+
+#include "rex_std/std_alias_defines.h"
+#include "rex_std/disable_std_checking.h"
+
+#include <stop_token>
+
+namespace rsl
+{
+    REX_STD_CLASS_ALIAS(stop_token);
+    REX_STD_CLASS_ALIAS(stop_source);
+
+    template <typename Callback>
+    REX_STD_TEMPLATED_CLASS_ALIAS(stop_callback, Callback);
+    
+    REX_STD_CLASS_ALIAS(nostopstate_t);
+
+    REX_STD_OBJECT_ALIAS(nostopstate);
+}
+
+#include "rex_std/enable_std_checking.h"
+
+#endif // REX_ENABLE_WITH_CPP20
