4e54a2442f7ec79ecf8e281c7bba9632e05790be
Nick De Breuck <nick.debreuck@outlook.com>
2022-08-31 01:11:57 +0100

    added tuple header

---
+++ source/include/0_extern/rex_std/tuple.h
@@ -0,0 +1,16 @@
+// ============================================
+//
+// REX - STANDARD LIBRARY IMPLEMENTATION
+//
+// Author: Nick De Breuck
+// Twitter: @nick_debreuck
+// 
+// File: tuple.h
+// Copyright (c) Nick De Breuck 2022
+//
+// ============================================
+
+#pragma once
+
+#include "rex_std/enable_std_checking.h"
+#include <tuple>
