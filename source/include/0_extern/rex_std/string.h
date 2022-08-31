2e59231905aad7e0c70044790a9f9fbca41e8181
Nick De Breuck <nick.debreuck@outlook.com>
2022-08-31 01:11:10 +0100

    added string header

---
+++ source/include/0_extern/rex_std/string.h
@@ -0,0 +1,18 @@
+// ============================================
+//
+// REX - STANDARD LIBRARY IMPLEMENTATION
+//
+// Author: Nick De Breuck
+// Twitter: @nick_debreuck
+// 
+// File: string.h
+// Copyright (c) Nick De Breuck 2022
+//
+// ============================================
+
+#pragma once
+
+#include "rex_std/internal/config.h"
+
+#include "rex_std/internal/string/char_traits.h"
+#include "rex_std/internal/string/basic_string.h"
