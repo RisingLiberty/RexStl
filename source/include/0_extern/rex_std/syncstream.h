d3bfef04b407ead19c6c0b41860d2b19ba56366c
Nick De Breuck <nick.debreuck@outlook.com>
2022-08-31 01:11:24 +0100

    added syncstream header

---
+++ source/include/0_extern/rex_std/syncstream.h
@@ -0,0 +1,45 @@
+// ============================================
+//
+// REX - STANDARD LIBRARY IMPLEMENTATION
+//
+// Author: Nick De Breuck
+// Twitter: @nick_debreuck
+// 
+// File: syncstream.h
+// Copyright (c) Nick De Breuck 2022
+//
+// ============================================
+
+#pragma once
+
+#ifdef REX_ENABLE_WITH_CPP20
+
+#include "rex_std/ostream.h"
+
+#include "rex_std/internal/memory/allocator.h"
+#include "rex_std/internal/string/char_traits.h"
+
+#include "rex_std/std_alias_defines.h"
+#include "rex_std/disable_std_checking.h"
+
+#include <syncstream>
+
+namespace rsl
+{    
+    template <typename CharT, typename Traits = char_traits<CharT>, typename Allocator = typed_allocator<CharT>>
+    REX_STD_TEMPLATED_CLASS_ALIAS(basic_syncbuf, CharT, Traits, Allocator);
+
+    template <typename CharT, typename Traits = char_traits<CharT>, typename Allocator = typed_allocator<CharT>>
+    REX_STD_TEMPLATED_CLASS_ALIAS(basic_osyncstream, CharT, Traits, Allocator);
+
+    REX_STD_CLASS_ALIAS(syncbuf);
+    REX_STD_CLASS_ALIAS(wsyncbuf);
+    REX_STD_CLASS_ALIAS(osyncstream);
+    REX_STD_CLASS_ALIAS(wosyncstream);
+
+    //REX_STD_FUNC_ALIAS(swap); // Doesn't work because rsl has its own swap
+}
+
+#include "rex_std/enable_std_checking.h"
+
+#endif // REX_ENABLE_WITH_CPP20
