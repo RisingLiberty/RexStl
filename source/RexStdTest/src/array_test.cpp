55229cc168316c958fc86dd8f9486c45d969f63d
Nick De Breuck <nick.debreuck@outlook.com>
2022-10-12 10:41:51 +0100

    added tests for rsl::array

---
+++ source/RexStdTest/src/array_test.cpp
@@ -0,0 +1,57 @@
+// ============================================
+//
+// REX - STANDARD LIBRARY IMPLEMENTATION
+//
+// Author: Nick De Breuck
+// Twitter: @nick_debreuck
+//
+// File: array_test.cpp
+// Copyright (c) Nick De Breuck 2022
+//
+// ============================================
+
+//#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
+#include "rex_std/array.h"
+
+TEST_CASE("array construction")
+{
+  const rsl::array<int, 10> arr;
+  REQUIRE(arr.size() == 10);
+
+  const rsl::array<int, 10> arr2 = { 1,2,3 };
+  REQUIRE(arr.size() == 10);
+}
+
+TEST_CASE("array assignment")
+{
+  rsl::array<int, 10> arr;
+  const rsl::array<int, 10> arr2;
+
+  REQUIRE(arr.size() == 10);
+  REQUIRE(arr2.size() == 10);
+  
+  arr = arr2;
+
+  REQUIRE(arr.size() == 10);
+  REQUIRE(arr2.size() == 10);
+}
+
+TEST_CASE("array element access")
+{
+  rsl::array<int, 10> arr;
+  arr[0] = 1;
+  arr[1] = 2;
+  arr[2] = 3;
+
+  REQUIRE(arr[0] == 1);
+  REQUIRE(arr[1] == 2);
+  REQUIRE(arr[2] == 3);
+
+  const rsl::array<int, 10> arr2 = { 1, 2, 3 };
+  REQUIRE(arr[0] == 1);
+  REQUIRE(arr[1] == 2);
+  REQUIRE(arr[2] == 3);
+  REQUIRE(arr[3] == 0);
+}
