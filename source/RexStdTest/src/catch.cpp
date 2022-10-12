0d3053c367df98b49b7614e57368c519db21a187
Nick De Breuck <nick.debreuck@outlook.com>
2022-10-12 10:41:41 +0100

    moved catch code implementation into its own cpp file

---
+++ source/RexStdTest/src/catch.cpp
@@ -0,0 +1,14 @@
+// ============================================
+//
+// REX - STANDARD LIBRARY IMPLEMENTATION
+//
+// Author: Nick De Breuck
+// Twitter: @nick_debreuck
+//
+// File: catch.cpp
+// Copyright (c) Nick De Breuck 2022
+//
+// ============================================
+
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
