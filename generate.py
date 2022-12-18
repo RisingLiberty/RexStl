105e80c73ebbda3c8da7798fb6514815a8d6b76c
Nick De Breuck <nick.debreuck@outlook.com>
2022-12-18 22:02:55 +0000

    fix bug where invalid arguments were passed to the generate script

--- generate.py
+++ generate.py
@@ -10,12 +10,12 @@ def run(generateUnitTests, lightMode):
   if lightMode:
     sharpmakeArgs += " /vsOnly"
 
-  os.system(f"py build/scripts/generate.py -sharpmake_main=build/sharpmake/src/main.sharpmake.cs -sharpmake_args={sharpmakeArgs}")
+  os.system(f"py build/scripts/generate.py -sharpmake_main=build/sharpmake/src/main.sharpmake.cs -sharpmake_args=\"{sharpmakeArgs}\"")
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
 
-  parser.add_argument("-generate_unittests", help="generate unit tests")
+  parser.add_argument("-generate_unittests", help="generate unit tests", action="store_true")
   parser.add_argument("-light", help="run in light mode", action="store_true")
 
   args, unknown = parser.parse_known_args()
