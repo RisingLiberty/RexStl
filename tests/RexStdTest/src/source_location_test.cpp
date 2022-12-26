ab0cb8735d0c8b39165c15e72b7f349e343956c7
Nick De Breuck <nick.debreuck@outlook.com>
2022-12-26 01:16:18 +0000

    added more code coverage of source_location

--- tests/RexStdTest/src/source_location_test.cpp
+++ tests/RexStdTest/src/source_location_test.cpp
@@ -15,15 +15,29 @@
 // NOLINTBEGIN
 
 #include "rex_std/source_location.h"
+#include "rex_std/string_view.h"
+#include "rex_std/bonus/compiler.h"
+#include "rex_std/iostream.h"
 
 TEST_CASE("source location")
 {
   rsl::source_location loc  = rsl::source_location::current();
   rsl::source_location loc2 = rsl::source_location::current();
+  rsl::string_view file = __FILE__;
+  rsl::string_view func = __func__;
+  rsl::string_view loc_file(loc.file_name());
+  rsl::string_view loc_func(loc.function_name());
+  
+  #if defined(REX_COMPILER_MSVC)
+  card32 column = 53;
+  #elif defined(REX_COMPILER_CLANG)
+  card32 column = 31;
+  #endif
+  
   REQUIRE(loc.line() + 1 == loc2.line());
-  REQUIRE(loc.column() == 2);
-  REQUIRE(loc.file_name() == __FILE__);
-  REQUIRE(loc.function_name() == __FUNCSIG__);
+  REQUIRE(loc.column() == column);
+  REQUIRE(loc_file == file);
+  REQUIRE(loc_func == func);
 
 }
 
