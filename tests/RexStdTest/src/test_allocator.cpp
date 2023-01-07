fee531d96ee2a7c0398b4791bc10fbbf53b57343
Nick De Breuck <nick.debreuck@outlook.com>
2023-01-07 21:42:35 +0000

    test allocator only executes deallocation when ptr is not nullptr

--- tests/RexStdTest/src/test_allocator.cpp
+++ tests/RexStdTest/src/test_allocator.cpp
@@ -56,6 +56,11 @@ namespace rsl::test
     }
     void test_allocator::deallocate(void* const ptr, size_type size) // NOLINT(readability-convert-member-functions-to-static)
     {
+      if (ptr == nullptr)
+      {
+        return;
+      }
+
       m_num_frees++;
       m_num_bytes_allocated -= static_cast<card32>(size);
       s_all_num_frees++;
