477b07352102ba0effcd90a01873f6612473a242
Nick De Breuck <nick.debreuck@outlook.com>
2023-01-18 09:48:48 +0000

    added rexstdextra to sharpmake scripts

---
+++ source/rex_std_extra/rexstdextra.sharpmake.cs
@@ -0,0 +1,37 @@
+using Sharpmake;
+using System.IO;
+
+[Generate]
+public class RexStandardExtraLibrary : BasicCPPProject
+{
+  public RexStandardExtraLibrary() : base()
+  {
+    // The name of the project in Visual Studio. The default is the name of
+    // the class, but you usually want to override that.
+    Name = GenerateName("RexStdExtra");
+    GenerateTargets();
+
+    // The directory that contains the source code we want to build is the
+    // same as this one. This string essentially means "the directory of
+    // the script you're reading right now."
+    string ThisFileFolder = Path.GetDirectoryName(Utils.CurrentFile());
+    SourceRootPath = ThisFileFolder;
+  }
+
+  public override void Configure(RexConfiguration conf, RexTarget target)
+  {
+    base.Configure(conf, target);
+
+    conf.Output = Configuration.OutputType.Lib;
+
+    if (target.Compiler == Compiler.Clang && conf.is_config_for_testing() == false)
+    {
+      conf.NinjaGenerateCompilerDB = true;
+      string compdbPath = Path.Combine(conf.ProjectPath, "clang_tools", target.Compiler.ToString(), conf.Name);
+      string postbuildCommandScript = Path.Combine(Globals.SourceRoot, $"post_build.py -p={Name} -comp={target.Compiler} -conf={conf.Name} -compdb={compdbPath}");
+      conf.EventPostBuild.Add(postbuildCommandScript);
+    }
+
+    conf.add_dependency<RexStd>(target);
+  }
+}
