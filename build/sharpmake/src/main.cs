d4371702b7f4a22dcadf975f663ae37608784855
Nick De Breuck <nick.debreuck@outlook.com>
2023-03-10 18:51:31 +0000

    [main] renamed commandline argument /noCompilerDB to /noClangTools

--- build/sharpmake/src/main.cs
+++ build/sharpmake/src/main.cs
@@ -657,6 +657,6 @@ public static class Main
     GenerateSettings.UndefinedBehaviorSanitizerEnabled = CommandLine.GetParameters().ToList().FindIndex(x => x.ToString() == "/enableUBSanitizer") != -1;
     GenerateSettings.FuzzyTestingEnabled = CommandLine.GetParameters().ToList().FindIndex(x => x.ToString() == "/enableFuzzyTesting") != -1;
     GenerateSettings.VisualStudioOnly = CommandLine.GetParameters().ToList().FindIndex(x => x.ToString() == "/vsOnly") != -1;
-    GenerateSettings.NoCompilerDB = CommandLine.GetParameters().ToList().FindIndex(x => x.ToString() == "/noCompilerDB") != -1;
+    GenerateSettings.NoCompilerDB = CommandLine.GetParameters().ToList().FindIndex(x => x.ToString() == "/noClangTools") != -1;
   }
 }
