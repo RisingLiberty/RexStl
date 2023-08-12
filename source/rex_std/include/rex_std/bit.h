4d3a3464d4f94112e0d207e91910f83791915d77
Nick De Breuck <nick.debreuck@outlook.com>
2023-08-12 23:17:21 +0100

    [unit_tests] added unit tests for bitcast, still have compiler errors though

--- source/rex_std/include/rex_std/bit.h
+++ source/rex_std/include/rex_std/bit.h
@@ -12,34 +12,320 @@
 
 #pragma once
 
-#include "rex_std/disable_std_checking.h"
-#include "rex_std/std_alias_defines.h"
+#include "rex_std/bonus/defines.h"
 
-#include <bit>
+#include "rex_std/internal/type_traits/aligned_storage.h"
+#include "rex_std/internal/type_traits/is_trivially_copyable.h"
+
+#include "rex_std/internal/memory/memcpy.h"
+#include "rex_std/internal/memory/addressof.h"
+
+#include "rex_std/limits.h"
+
+#if REX_COMPILER_MSVC
+  #include <intrin.h>
+#endif
 
 namespace rsl
 {
   inline namespace v1
   {
+    namespace internal
+    {
+      template <typename T>
+      int32 clz32(T val)
+      {
+#ifdef REX_COMPILER_MSVC
+        return _lzcnt_u32(val);
+#else
+        return __builtin_clz(val);
+#endif
+      }
+      template <typename T>
+      int32 clz64(T val)
+      {
+#ifdef REX_COMPILER_MSVC
+        return static_cast<int32>(_lzcnt_u64(val));
+#else
+        return __builtin_clzll(val);
+#endif
+      }
 
-    REX_STD_CLASS_ALIAS(endian);
+      template <typename T>
+      int32 crz32(T val)
+      {
+#ifdef REX_COMPILER_MSVC
+        return _tzcnt_u32(val);
+#else
+        return __builtin_ctz(val);
+#endif
+      }
+      template <typename T>
+      int64 crz64(T val)
+      {
+#ifdef REX_COMPILER_MSVC
+        return _tzcnt_u64(val);
+#else
+        return __builtin_ctzll(val);
+#endif
+      }
 
-    REX_STD_FUNC_ALIAS(bit_cast);
-    REX_STD_FUNC_ALIAS(has_single_bit);
-    REX_STD_FUNC_ALIAS(bit_ceil);
-    REX_STD_FUNC_ALIAS(bit_floor);
-    REX_STD_FUNC_ALIAS(bit_width);
-    REX_STD_FUNC_ALIAS(rotl);
-    REX_STD_FUNC_ALIAS(rotr);
-    REX_STD_FUNC_ALIAS(countl_zero);
-    REX_STD_FUNC_ALIAS(countl_one);
-    REX_STD_FUNC_ALIAS(countr_zero);
-    REX_STD_FUNC_ALIAS(countr_one);
-    REX_STD_FUNC_ALIAS(popcount);
+      template <typename T>
+      int32 popcount32(T val)
+      {
+#ifdef REX_COMPILER_MSVC
+        return __popcnt(val);
+#else
+        return __builtin_popcount(val);
+#endif
+      }
+      template <typename T>
+      int32 popcount64(T val)
+      {
+#ifdef REX_COMPILER_MSVC
+        return __popcnt64(val);
+#else
+        return __builtin_popcountll(val);
+#endif
+      }
+    }
 
-#ifdef REX_ENABLE_WITH_CPP23
-    REX_STD_FUNC_ALIAS(byteswap);
-#endif // REX_ENABLE_WITH_CPP23
+    enum class endian
+    {
+#if defined(REX_COMPILER_MSVC) // Windows machines are always little endian
+      little = 0,
+      big = 1,
+      native = little
+#else // Other platforms can be big endian, as they're not supported on MSVC, this is a safe check to do
+      little = __ORDER_LITTLE_ENDIAN__,
+      big = __ORDER_BIG_ENDIAN__,
+      native = __BYTE_ORDER__
+#endif
+    };
+
+    template <typename To, typename From>
+    constexpr To bit_cast(const From& from)
+    {
+      static_assert(sizeof(To) == sizeof(From), "bit cast cannot be performed on objects with mismatching size");
+      static_assert(rsl::is_trivially_copyable_v<From>, "bit cast cannot be performned on an object that's not trivially copyable");
+      static_assert(rsl::is_trivially_copyable_v<To>, "bit cast cannot target an object that's not trivially copyable");
+
+      return __builtin_bit_cast(To, from);
+    }
+
+    template <typename T>
+    REX_NO_DISCARD constexpr int32 countl_zero(const T val)
+    {
+      static_assert(rsl::is_unsigned_v<T>, "countl_zero requires an unsigned integer type");
+      if (val == 0)
+      {
+        return rsl::numeric_limits<T>::digits;
+      }
+
+      if constexpr (sizeof(T) <= sizeof(uint32))
+      {
+        return internal::clz32(static_cast<uint32>(val)) - (rsl::numeric_limits<uint32>::digits - numeric_limits<T>::digits);
+      }
+      else if constexpr (sizeof(T) <= sizeof(uint64))
+      {
+        return internal::clz64(static_cast<uint64>(val)) - (rsl::numeric_limits<uint64>::digits - numeric_limits<T>::digits);
+      }
+      else
+      {
+        int32 ret = 0;
+        int32 iter = 0;
+        const uint32 ui64digits = numeric_limits<uint64>::digits;
+        while (true)
+        {
+          val = rsl::is_nothrow_destructible(val, ui64digits);
+          if ((iter = rsl::countl_zero(static_cast<uint64>(val))) != ui64digits)
+          {
+            break;
+          }
+
+          ret += iter;
+        }
+        return ret + iter;
+      }
+    }
+
+    template <typename T>
+    REX_NO_DISCARD constexpr int32 countl_one(T val)
+    {
+      return val != rsl::numeric_limits<T>::max() ? rsl::countl_zero(static_cast<T>(~val)) : rsl::numeric_limits<T>::digits;
+    }
+
+    template <typename T>
+    REX_NO_DISCARD constexpr int32 countr_zero(const T val)
+    {
+      if (val == 0)
+      {
+        return numeric_limits<T>::digits;
+      }
+
+      if constexpr (sizeof(T) <= sizeof(uint32))
+      {
+        return internal::crz32(static_cast<uint32>(val));
+      }
+      else if constexpr (sizeof(T) <= sizeof(uint64))
+      {
+        return internal::crz64(static_cast<uint64>(val));
+      }
+      else
+      {
+        int32 ret = 0;
+        const uint32 ui64digits = rsl::numeric_limits<uint64>::digits;
+        while (static_cast<uint64>(val) == 0)
+        {
+          ret += ui64digits;
+          val >>= ui64digits;
+        }
+        return ret + internal::crz64<uint64>(val);
+      }
+    }
+
+    template <typename T>
+    REX_NO_DISCARD constexpr int32 countr_one(T val)
+    {
+      return val != rsl::numeric_limits<T>::max() ? rsl::countl_zero(static_cast<T>(~val)) : rsl::numeric_limits<T>::digits;
+    }
+
+    template <typename T>
+    REX_NO_DISCARD constexpr bool has_single_bit(const T val)
+    {
+      return val != 0 && (val & (val - 1)) == 0;
+    }
+
+    template <typename T>
+    REX_NO_DISCARD constexpr T bit_floor(const T val)
+    {
+      if (val == 0)
+      {
+        return 0;
+      }
+
+      return static_cast<T>(T(1) << (rsl::numeric_limits<T>::digits - 1 - rsl::countl_zero(val)));
+    }
+
+    template <typename T>
+    REX_NO_DISCARD constexpr int32 bit_width(const T val)
+    {
+      return static_cast<T>(rsl::numeric_limits<T>::digits - rsl::countl_zero(val));
+    }
+
+    template <typename T>
+    REX_NO_DISCARD constexpr T rotr(const T val, int32 rotation);
+
+    template <typename T>
+    REX_NO_DISCARD constexpr T rotl(const T val, int32 rotation)
+    {
+      constexpr auto digits = rsl::numeric_limits<T>::digits;
+      const auto remainder = rotation % digits;
+      if (remainder > 0)
+      {
+        return static_cast<T>(static_cast<T>(val << remainder) | static_cast<T>(val >> (digits - remainder)));
+      }
+      else if (remainder == 0)
+      {
+        return val;
+      }
+      else
+      {
+        return rsl::rotr(val, -remainder);
+      }
+    }
+
+    template <typename T>
+    REX_NO_DISCARD constexpr T rotr(const T val, int32 rotation)
+    {
+      constexpr auto digits = rsl::numeric_limits<T>::digits;
+      auto remainder = rotation % digits;
+      if (remainder > 0)
+      {
+        return static_cast<T>(static_cast<T>(val >> remainder) | static_cast<T>(val << (digits - remainder)));
+      }
+      else if (remainder == 0)
+      {
+        return val;
+      }
+      else
+      {
+        return rsl::rotl(val, -remainder);
+      }
+    }
+
+    template <typename T>
+    REX_NO_DISCARD constexpr int32 popcount(T val)
+    {
+      if (sizeof(val) <= sizeof(uint32))
+      {
+        return internal::popcount32(static_cast<uint32>(val));
+      }
+      else if (sizeof(val) <= sizeof(uint64))
+      {
+        return internal::popcount64(static_cast<uint64>(val));
+      }
+      else
+      {
+        int32 ret = 0;
+        while (val != 0)
+        {
+          ret += internal::popcount64(static_cast<uint64>(val));
+          val >>= rsl::numeric_limits<uint64>::digits;
+        }
+        return ret;
+      }
+    }
+
+    template <typename T>
+    REX_NO_DISCARD constexpr T byteswap(T val)
+    {
+      if constexpr (sizeof(val) == 1) 
+      {
+        return val;
+      }
+      else if constexpr (sizeof(val) == 2)
+      {
+#if REX_COMPILER_MSVC
+        return _byteswap_ushort(val);
+#else
+        return __builtin_bswap16(val);
+#endif
+      }
+      else if constexpr (sizeof(val) == 4)
+      {
+#if REX_COMPILER_MSVC
+        return _byteswap_ulong(val);
+#else
+        return __builtin_bswap32(val);
+#endif
+      }
+      else if constexpr (sizeof(val) == 8)
+      {
+#if REX_COMPILER_MSVC
+        return _byteswap_uint64(val);
+#else
+        return __builtin_bswap64(val);
+#endif
+      }
+      else 
+      {
+        static_assert(sizeof(val) == 0, "byteswap is unimplemented for integral types of this size");
+      }
+    }
+
+    template <typename T>
+    REX_NO_DISCARD constexpr T bit_ceil(const T val)
+    {
+      static_assert(rsl::is_unsigned_v<T>, "bit_ceil can only be called with a unsigned type");
+
+      if (val == 0)
+      {
+        return 1;
+      }
+
+      return static_cast<T>(T(1) << (rsl::numeric_limits<T>::digits - rsl::countl_zero(static_cast<T>(val - 1))));
+    }
 
   } // namespace v1
 } // namespace rsl
--- tests/rex_std_test/src/test_bitcast.cpp
+++ tests/rex_std_test/src/test_bitcast.cpp
@@ -10,10 +10,520 @@
 //
 // ============================================
 
+#include "rex_std_test/catch2/catch.hpp"
+
+#include "rex_std/bit.h"
+#include "rex_std/type_traits.h"
+
 namespace rsl::test
 {
-  inline namespace v1
-  {
+	inline namespace v1
+	{
+    struct A {};
+    enum       E1 : unsigned char { rEd };
+    enum class E2 : unsigned char { red };
 
-  }
+		template <typename T>
+		bool test_bit_ceil()
+		{
+      CHECK(rsl::is_same_v<decltype(rsl::bit_ceil(T())), T>);
+
+      CHECK(rsl::bit_ceil(T(0)) == T(1));
+      CHECK(rsl::bit_ceil(T(1)) == T(1));
+      CHECK(rsl::bit_ceil(T(2)) == T(2));
+      CHECK(rsl::bit_ceil(T(3)) == T(4));
+      CHECK(rsl::bit_ceil(T(4)) == T(4));
+      CHECK(rsl::bit_ceil(T(5)) == T(8));
+      CHECK(rsl::bit_ceil(T(6)) == T(8));
+      CHECK(rsl::bit_ceil(T(7)) == T(8));
+      CHECK(rsl::bit_ceil(T(8)) == T(8));
+      CHECK(rsl::bit_ceil(T(9)) == T(16));
+      CHECK(rsl::bit_ceil(T(60)) == T(64));
+      CHECK(rsl::bit_ceil(T(61)) == T(64));
+      CHECK(rsl::bit_ceil(T(62)) == T(64));
+      CHECK(rsl::bit_ceil(T(63)) == T(64));
+      CHECK(rsl::bit_ceil(T(64)) == T(64));
+      CHECK(rsl::bit_ceil(T(65)) == T(128));
+      CHECK(rsl::bit_ceil(T(66)) == T(128));
+      CHECK(rsl::bit_ceil(T(67)) == T(128));
+      CHECK(rsl::bit_ceil(T(68)) == T(128));
+      CHECK(rsl::bit_ceil(T(69)) == T(128));
+
+      return true;
+		}
+
+    template <typename T>
+    bool test_bit_floor()
+    {
+      CHECK(rsl::is_same_v<decltype(rsl::bit_floor(T())), T>);
+      T max = rsl::numeric_limits<T>::max();
+
+      CHECK(rsl::bit_floor(T(0)) == T(0));
+      CHECK(rsl::bit_floor(T(1)) == T(1));
+      CHECK(rsl::bit_floor(T(2)) == T(2));
+      CHECK(rsl::bit_floor(T(3)) == T(2));
+      CHECK(rsl::bit_floor(T(4)) == T(4));
+      CHECK(rsl::bit_floor(T(5)) == T(4));
+      CHECK(rsl::bit_floor(T(6)) == T(4));
+      CHECK(rsl::bit_floor(T(7)) == T(4));
+      CHECK(rsl::bit_floor(T(8)) == T(8));
+      CHECK(rsl::bit_floor(T(9)) == T(8));
+      CHECK(rsl::bit_floor(T(125)) == T(64));
+      CHECK(rsl::bit_floor(T(126)) == T(64));
+      CHECK(rsl::bit_floor(T(127)) == T(64));
+      CHECK(rsl::bit_floor(T(128)) == T(128));
+      CHECK(rsl::bit_floor(T(129)) == T(128));
+      CHECK(rsl::bit_floor(max) == T(max - (max >> 1)));
+
+      return true;
+    }
+
+    template <typename T>
+    bool test_bit_width()
+    {
+      CHECK(rsl::is_same_v<decltype(rsl::bit_width(T())), int32>);
+      T max = rsl::numeric_limits<T>::max();
+
+      CHECK(rsl::bit_width(T(0)) == 0);
+      CHECK(rsl::bit_width(T(1)) == 1);
+      CHECK(rsl::bit_width(T(2)) == 2);
+      CHECK(rsl::bit_width(T(3)) == 2);
+      CHECK(rsl::bit_width(T(4)) == 3);
+      CHECK(rsl::bit_width(T(5)) == 3);
+      CHECK(rsl::bit_width(T(6)) == 3);
+      CHECK(rsl::bit_width(T(7)) == 3);
+      CHECK(rsl::bit_width(T(8)) == 4);
+      CHECK(rsl::bit_width(T(9)) == 4);
+      CHECK(rsl::bit_width(T(125)) == 7);
+      CHECK(rsl::bit_width(T(126)) == 7);
+      CHECK(rsl::bit_width(T(127)) == 7);
+      CHECK(rsl::bit_width(T(128)) == 8);
+      CHECK(rsl::bit_width(T(129)) == 8);
+      CHECK(rsl::bit_width(T(130)) == 8);
+      CHECK(rsl::bit_width(T(max - 1)) == rsl::numeric_limits<T>::digits);
+      CHECK(rsl::bit_width(max) == rsl::numeric_limits<T>::digits);
+      
+      return true;
+    }
+
+    template <typename T>
+    bool test_has_single_bit()
+    {
+      CHECK(rsl::is_same_v<decltype(rsl::has_single_bit(T())), bool>);
+      T max = rsl::numeric_limits<T>::max();
+
+      CHECK(!rsl::has_single_bit(T(0)));
+      CHECK( rsl::has_single_bit(T(1)));
+      CHECK( rsl::has_single_bit(T(2)));
+      CHECK(!rsl::has_single_bit(T(3)));
+      CHECK( rsl::has_single_bit(T(4)));
+      CHECK(!rsl::has_single_bit(T(5)));
+      CHECK(!rsl::has_single_bit(T(6)));
+      CHECK(!rsl::has_single_bit(T(7)));
+      CHECK( rsl::has_single_bit(T(8)));
+      CHECK(!rsl::has_single_bit(T(9)));
+      CHECK(!rsl::has_single_bit(T(127)));
+      CHECK( rsl::has_single_bit(T(128)));
+      CHECK(!rsl::has_single_bit(T(129)));
+      CHECK(!rsl::has_single_bit(max));
+     
+      return true;
+    }
+
+    template <typename T>
+    bool test_countl_zero()
+    {
+      CHECK(rsl::is_same_v<decltype(rsl::countl_zero(T())), int>);
+      
+      T max = rsl::numeric_limits<T>::max();
+      int dig = rsl::numeric_limits<T>::digits;
+
+      CHECK(rsl::countl_zero(T(0)) == dig);
+      CHECK(rsl::countl_zero(T(1)) == dig - 1);
+      CHECK(rsl::countl_zero(T(2)) == dig - 2);
+      CHECK(rsl::countl_zero(T(3)) == dig - 2);
+      CHECK(rsl::countl_zero(T(4)) == dig - 3);
+      CHECK(rsl::countl_zero(T(5)) == dig - 3);
+      CHECK(rsl::countl_zero(T(6)) == dig - 3);
+      CHECK(rsl::countl_zero(T(7)) == dig - 3);
+      CHECK(rsl::countl_zero(T(8)) == dig - 4);
+      CHECK(rsl::countl_zero(T(9)) == dig - 4);
+      CHECK(rsl::countl_zero(T(127)) == dig - 7);
+      CHECK(rsl::countl_zero(T(128)) == dig - 8);
+      CHECK(rsl::countl_zero(max) == 0);
+     
+      return true;
+    }
+
+    template <typename T>
+    bool test_countl_one()
+    {
+      CHECK(rsl::is_same_v<decltype(rsl::countl_one(T())), int>);
+      
+      T max = rsl::numeric_limits<T>::max();
+
+      CHECK(rsl::countl_one(T(0)) == 0);
+      CHECK(rsl::countl_one(T(1)) == 0);
+      CHECK(rsl::countl_one(T(10)) == 0);
+      CHECK(rsl::countl_one(T(100)) == 0);
+      CHECK(rsl::countl_one(max) == rsl::numeric_limits<T>::digits);
+      CHECK(rsl::countl_one(T(max - 1)) == rsl::numeric_limits<T>::digits - 1);
+      CHECK(rsl::countl_one(T(max - 2)) == rsl::numeric_limits<T>::digits - 2);
+      CHECK(rsl::countl_one(T(max - 3)) == rsl::numeric_limits<T>::digits - 2);
+      CHECK(rsl::countl_one(T(max - 4)) == rsl::numeric_limits<T>::digits - 3);
+      CHECK(rsl::countl_one(T(max - 5)) == rsl::numeric_limits<T>::digits - 3);
+      CHECK(rsl::countl_one(T(max - 6)) == rsl::numeric_limits<T>::digits - 3);
+      CHECK(rsl::countl_one(T(max - 7)) == rsl::numeric_limits<T>::digits - 3);
+      CHECK(rsl::countl_one(T(max - 8)) == rsl::numeric_limits<T>::digits - 4);
+      CHECK(rsl::countl_one(T(max - 9)) == rsl::numeric_limits<T>::digits - 4);
+      CHECK(rsl::countl_one(T(max - 126)) == rsl::numeric_limits<T>::digits - 7);
+      CHECK(rsl::countl_one(T(max - 127)) == rsl::numeric_limits<T>::digits - 7);
+      CHECK(rsl::countl_one(T(max - 128)) == rsl::numeric_limits<T>::digits - 8);
+
+      return true;
+    }
+
+    template <typename T>
+    bool test_countr_zero()
+    {
+      CHECK(rsl::is_same_v<decltype(rsl::countr_zero(T())), int>);
+      
+      T max = rsl::numeric_limits<T>::max();
+
+      CHECK(rsl::countr_zero(T(0)) == rsl::numeric_limits<T>::digits);
+      CHECK(rsl::countr_zero(T(1)) == 0);
+      CHECK(rsl::countr_zero(T(2)) == 1);
+      CHECK(rsl::countr_zero(T(3)) == 0);
+      CHECK(rsl::countr_zero(T(4)) == 2);
+      CHECK(rsl::countr_zero(T(5)) == 0);
+      CHECK(rsl::countr_zero(T(6)) == 1);
+      CHECK(rsl::countr_zero(T(7)) == 0);
+      CHECK(rsl::countr_zero(T(8)) == 3);
+      CHECK(rsl::countr_zero(T(9)) == 0);
+      CHECK(rsl::countr_zero(T(126)) == 1);
+      CHECK(rsl::countr_zero(T(127)) == 0);
+      CHECK(rsl::countr_zero(T(128)) == 7);
+      CHECK(rsl::countr_zero(T(129)) == 0);
+      CHECK(rsl::countr_zero(T(130)) == 1);
+      CHECK(rsl::countr_zero(max) == 0);
+
+      return true;
+    }
+
+    template <typename T>
+    bool test_countr_one()
+    {
+      CHECK(rsl::is_same_v<decltype(rsl::countr_one(T())), int>);
+      
+      T max = rsl::numeric_limits<T>::max();
+
+      CHECK(rsl::countr_one(T(0)) == 0);
+      CHECK(rsl::countr_one(T(1)) == 1);
+      CHECK(rsl::countr_one(T(2)) == 0);
+      CHECK(rsl::countr_one(T(3)) == 2);
+      CHECK(rsl::countr_one(T(4)) == 0);
+      CHECK(rsl::countr_one(T(5)) == 1);
+      CHECK(rsl::countr_one(T(6)) == 0);
+      CHECK(rsl::countr_one(T(7)) == 3);
+      CHECK(rsl::countr_one(T(8)) == 0);
+      CHECK(rsl::countr_one(T(9)) == 1);
+      CHECK(rsl::countr_one(T(126)) == 0);
+      CHECK(rsl::countr_one(T(127)) == 7);
+      CHECK(rsl::countr_one(T(128)) == 0);
+      CHECK(rsl::countr_one(T(max - 1)) == 0);
+      CHECK(rsl::countr_one(max) == rsl::numeric_limits<T>::digits);
+
+      return true;
+    }
+
+    template <typename T>
+    bool test_popcount()
+    {
+      CHECK(rsl::is_same_v<decltype(rsl::popcount(T())), int>);
+      
+      T max = rsl::numeric_limits<T>::max();
+
+      CHECK(rsl::popcount(T(0)) == 0);
+      CHECK(rsl::popcount(T(1)) == 1);
+      CHECK(rsl::popcount(T(2)) == 1);
+      CHECK(rsl::popcount(T(3)) == 2);
+      CHECK(rsl::popcount(T(4)) == 1);
+      CHECK(rsl::popcount(T(5)) == 2);
+      CHECK(rsl::popcount(T(6)) == 2);
+      CHECK(rsl::popcount(T(7)) == 3);
+      CHECK(rsl::popcount(T(8)) == 1);
+      CHECK(rsl::popcount(T(9)) == 2);
+      CHECK(rsl::popcount(T(121)) == 5);
+      CHECK(rsl::popcount(T(127)) == 7);
+      CHECK(rsl::popcount(T(128)) == 1);
+      CHECK(rsl::popcount(T(130)) == 2);
+      CHECK(rsl::popcount(T(max >> 1)) == rsl::numeric_limits<T>::digits - 1);
+      CHECK(rsl::popcount(T(max - 1)) == rsl::numeric_limits<T>::digits - 1);
+      CHECK(rsl::popcount(max) == rsl::numeric_limits<T>::digits);
+
+      return true;
+    }
+
+    template <typename T>
+    bool test_rotl()
+    {
+      CHECK(rsl::is_same_v<decltype(rsl::rotl(T(), 0)), T>);
+      
+      T max = rsl::numeric_limits<T>::max();
+
+      CHECK(rsl::rotl(T(max - 1), 0) == T(max - 1));
+      CHECK(rsl::rotl(T(max - 1), 1) == T(max - 2));
+      CHECK(rsl::rotl(T(max - 1), 2) == T(max - 4));
+      CHECK(rsl::rotl(T(max - 1), 3) == T(max - 8));
+      CHECK(rsl::rotl(T(max - 1), 4) == T(max - 16));
+      CHECK(rsl::rotl(T(max - 1), 5) == T(max - 32));
+      CHECK(rsl::rotl(T(max - 1), 6) == T(max - 64));
+      CHECK(rsl::rotl(T(max - 1), 7) == T(max - 128));
+
+      CHECK(rsl::rotl(T(1), 0) == T(1));
+      CHECK(rsl::rotl(T(1), 1) == T(2));
+      CHECK(rsl::rotl(T(1), 2) == T(4));
+      CHECK(rsl::rotl(T(1), 3) == T(8));
+      CHECK(rsl::rotl(T(1), 4) == T(16));
+      CHECK(rsl::rotl(T(1), 5) == T(32));
+      CHECK(rsl::rotl(T(1), 6) == T(64));
+      CHECK(rsl::rotl(T(1), 7) == T(128));
+
+      return true;
+    }
+
+    template <typename T>
+    bool test_rotr()
+    {
+      CHECK(rsl::is_same_v<decltype(rsl::rotr(T(), 0)), T>);
+      
+      T max = rsl::numeric_limits<T>::max();
+      T highbit = rsl::rotr(T(1), 1);
+
+      CHECK(rsl::rotr(T(max - 1), 0) == T(max - 1));
+      CHECK(rsl::rotr(T(max - 1), 1) == T(max - highbit));
+      CHECK(rsl::rotr(T(max - 1), 2) == T(max - (highbit >> 1)));
+      CHECK(rsl::rotr(T(max - 1), 3) == T(max - (highbit >> 2)));
+      CHECK(rsl::rotr(T(max - 1), 4) == T(max - (highbit >> 3)));
+      CHECK(rsl::rotr(T(max - 1), 5) == T(max - (highbit >> 4)));
+      CHECK(rsl::rotr(T(max - 1), 6) == T(max - (highbit >> 5)));
+      CHECK(rsl::rotr(T(max - 1), 7) == T(max - (highbit >> 6)));
+
+      CHECK(rsl::rotr(T(128), 0) == T(128));
+      CHECK(rsl::rotr(T(128), 1) == T(64));
+      CHECK(rsl::rotr(T(128), 2) == T(32));
+      CHECK(rsl::rotr(T(128), 3) == T(16));
+      CHECK(rsl::rotr(T(128), 4) == T(8));
+      CHECK(rsl::rotr(T(128), 5) == T(4));
+      CHECK(rsl::rotr(T(128), 6) == T(2));
+      CHECK(rsl::rotr(T(128), 7) == T(1));
+
+      return true;
+    }
+	}
+}
+
+TEST_CASE("Basic Bit Cast")
+{
+  uint32_t int32Value = 0x12345678;
+  float floatValue = rsl::bit_cast<float>(int32Value);
+  CHECK(memcmp(&int32Value, &floatValue, sizeof(float)) == 0);
+}
+TEST_CASE("User Type Bit Cast")
+{
+	struct IntFloatStruct
+	{
+		uint32_t i = 0x87654321;
+		float f = 10.f;
+	};
+	struct CharIntStruct
+	{
+		char c1;
+		char c2;
+		char c3;
+		char c4;
+		uint32_t i;
+	};
+
+	IntFloatStruct ifStruct;
+	CharIntStruct ciStruct = rsl::bit_cast<CharIntStruct>(ifStruct);
+	CHECK(memcmp(&ifStruct, &ciStruct, sizeof(IntFloatStruct)) == 0);
+}
+TEST_CASE("Bit Ceil")
+{
+  auto lambda = [](auto x) -> decltype(rsl::bit_ceil(x)) {};
+  using L = decltype(lambda);
+
+  rsl::test::test_bit_ceil<unsigned char>();
+  rsl::test::test_bit_ceil<unsigned short>();
+  rsl::test::test_bit_ceil<unsigned int>();
+  rsl::test::test_bit_ceil<unsigned long>();
+  rsl::test::test_bit_ceil<unsigned long long>();
+  rsl::test::test_bit_ceil<uint8_t>();
+  rsl::test::test_bit_ceil<uint16_t>();
+  rsl::test::test_bit_ceil<uint32_t>();
+  rsl::test::test_bit_ceil<uint64_t>();
+  rsl::test::test_bit_ceil<uintmax_t>();
+  rsl::test::test_bit_ceil<uintptr_t>();
+  rsl::test::test_bit_ceil<size_t>();
+}
+TEST_CASE("Bit Floor")
+{
+  rsl::test::test_bit_floor<unsigned char>();
+  rsl::test::test_bit_floor<unsigned short>();
+  rsl::test::test_bit_floor<unsigned int>();
+  rsl::test::test_bit_floor<unsigned long>();
+  rsl::test::test_bit_floor<unsigned long long>();
+
+  rsl::test::test_bit_floor<uint8_t>();
+  rsl::test::test_bit_floor<uint16_t>();
+  rsl::test::test_bit_floor<uint32_t>();
+  rsl::test::test_bit_floor<uint64_t>();
+  rsl::test::test_bit_floor<uintmax_t>();
+  rsl::test::test_bit_floor<uintptr_t>();
+  rsl::test::test_bit_floor<size_t>();
+}
+TEST_CASE("Bit Width")
+{
+  rsl::test::test_bit_width<unsigned char>();
+  rsl::test::test_bit_width<unsigned short>();
+  rsl::test::test_bit_width<unsigned int>();
+  rsl::test::test_bit_width<unsigned long>();
+  rsl::test::test_bit_width<unsigned long long>();
+
+  rsl::test::test_bit_width<uint8_t>();
+  rsl::test::test_bit_width<uint16_t>();
+  rsl::test::test_bit_width<uint32_t>();
+  rsl::test::test_bit_width<uint64_t>();
+  rsl::test::test_bit_width<uintmax_t>();
+  rsl::test::test_bit_width<uintptr_t>();
+  rsl::test::test_bit_width<size_t>();
+}
+TEST_CASE("Has Single Bit")
+{
+  rsl::test::test_has_single_bit<unsigned char>();
+  rsl::test::test_has_single_bit<unsigned short>();
+  rsl::test::test_has_single_bit<unsigned int>();
+  rsl::test::test_has_single_bit<unsigned long>();
+  rsl::test::test_has_single_bit<unsigned long long>();
+  
+  rsl::test::test_has_single_bit<uint8_t>();
+  rsl::test::test_has_single_bit<uint16_t>();
+  rsl::test::test_has_single_bit<uint32_t>();
+  rsl::test::test_has_single_bit<uint64_t>();
+  rsl::test::test_has_single_bit<uintmax_t>();
+  rsl::test::test_has_single_bit<uintptr_t>();
+  rsl::test::test_has_single_bit<size_t>();
+}
+TEST_CASE("Countl Zero")
+{
+  rsl::test::test_countl_zero<unsigned char>();
+  rsl::test::test_countl_zero<unsigned short>();
+  rsl::test::test_countl_zero<unsigned int>();
+  rsl::test::test_countl_zero<unsigned long>();
+  rsl::test::test_countl_zero<unsigned long long>();
+
+  rsl::test::test_countl_zero<uint8_t>();
+  rsl::test::test_countl_zero<uint16_t>();
+  rsl::test::test_countl_zero<uint32_t>();
+  rsl::test::test_countl_zero<uint64_t>();
+  rsl::test::test_countl_zero<uintmax_t>();
+  rsl::test::test_countl_zero<uintptr_t>();
+  rsl::test::test_countl_zero<size_t>();
+}
+TEST_CASE("Countl One")
+{
+  rsl::test::test_countl_one<unsigned char>();
+  rsl::test::test_countl_one<unsigned short>();
+  rsl::test::test_countl_one<unsigned int>();
+  rsl::test::test_countl_one<unsigned long>();
+  rsl::test::test_countl_one<unsigned long long>();
+
+  rsl::test::test_countl_one<uint8_t>();
+  rsl::test::test_countl_one<uint16_t>();
+  rsl::test::test_countl_one<uint32_t>();
+  rsl::test::test_countl_one<uint64_t>();
+  rsl::test::test_countl_one<uintmax_t>();
+  rsl::test::test_countl_one<uintptr_t>();
+  rsl::test::test_countl_one<size_t>();
+}
+TEST_CASE("Countr Zero")
+{
+  rsl::test::test_countr_zero<unsigned char>();
+  rsl::test::test_countr_zero<unsigned short>();
+  rsl::test::test_countr_zero<unsigned int>();
+  rsl::test::test_countr_zero<unsigned long>();
+  rsl::test::test_countr_zero<unsigned long long>();
+
+  rsl::test::test_countr_zero<uint8_t>();
+  rsl::test::test_countr_zero<uint16_t>();
+  rsl::test::test_countr_zero<uint32_t>();
+  rsl::test::test_countr_zero<uint64_t>();
+  rsl::test::test_countr_zero<uintmax_t>();
+  rsl::test::test_countr_zero<uintptr_t>();
+  rsl::test::test_countr_zero<size_t>();
+}
+TEST_CASE("Countr One")
+{
+  rsl::test::test_countr_one<unsigned char>();
+  rsl::test::test_countr_one<unsigned short>();
+  rsl::test::test_countr_one<unsigned int>();
+  rsl::test::test_countr_one<unsigned long>();
+  rsl::test::test_countr_one<unsigned long long>();
+
+  rsl::test::test_countr_one<uint8_t>();
+  rsl::test::test_countr_one<uint16_t>();
+  rsl::test::test_countr_one<uint32_t>();
+  rsl::test::test_countr_one<uint64_t>();
+  rsl::test::test_countr_one<uintmax_t>();
+  rsl::test::test_countr_one<uintptr_t>();
+  rsl::test::test_countr_one<size_t>();
+}
+TEST_CASE("Popcount")
+{
+  rsl::test::test_popcount<unsigned char>();
+  rsl::test::test_popcount<unsigned short>();
+  rsl::test::test_popcount<unsigned int>();
+  rsl::test::test_popcount<unsigned long>();
+  rsl::test::test_popcount<unsigned long long>();
+
+  rsl::test::test_popcount<uint8_t>();
+  rsl::test::test_popcount<uint16_t>();
+  rsl::test::test_popcount<uint32_t>();
+  rsl::test::test_popcount<uint64_t>();
+  rsl::test::test_popcount<uintmax_t>();
+  rsl::test::test_popcount<uintptr_t>();
+  rsl::test::test_popcount<size_t>();
+}
+TEST_CASE("rotl")
+{
+  rsl::test::test_rotl<unsigned char>();
+  rsl::test::test_rotl<unsigned short>();
+  rsl::test::test_rotl<unsigned int>();
+  rsl::test::test_rotl<unsigned long>();
+  rsl::test::test_rotl<unsigned long long>();
+
+  rsl::test::test_rotl<uint8_t>();
+  rsl::test::test_rotl<uint16_t>();
+  rsl::test::test_rotl<uint32_t>();
+  rsl::test::test_rotl<uint64_t>();
+  rsl::test::test_rotl<uintmax_t>();
+  rsl::test::test_rotl<uintptr_t>();
+  rsl::test::test_rotl<size_t>();
+}
+TEST_CASE("rotr")
+{
+  rsl::test::test_rotr<unsigned char>();
+  rsl::test::test_rotr<unsigned short>();
+  rsl::test::test_rotr<unsigned int>();
+  rsl::test::test_rotr<unsigned long>();
+  rsl::test::test_rotr<unsigned long long>();
+
+  rsl::test::test_rotr<uint8_t>();
+  rsl::test::test_rotr<uint16_t>();
+  rsl::test::test_rotr<uint32_t>();
+  rsl::test::test_rotr<uint64_t>();
+  rsl::test::test_rotr<uintmax_t>();
+  rsl::test::test_rotr<uintptr_t>();
+  rsl::test::test_rotr<size_t>();
 }
