6dd3f82cc19c7159f8fc1f9c297fb2ba1cae3874
Nick De Breuck <nick.debreuck@outlook.com>
2024-01-02 22:17:58 +0000

    [main] adding unordered_set to rex stl

---
+++ source/rex_std/include/rex_std/bonus/hashtable/hash_set.h
@@ -0,0 +1,372 @@
+// ============================================
+//
+// REX - STANDARD LIBRARY IMPLEMENTATION
+//
+// Author: Nick De Breuck
+// Twitter: @nick_debreuck
+// 
+// File: hash_set.h
+// Copyright (c) Nick De Breuck 2022
+//
+// ============================================
+
+#pragma once
+
+#include "rex_std/internal/functional/hash.h"
+#include "rex_std/internal/functional/equal_to.h"
+#include "rex_std/internal/memory/allocator.h"
+#include "rex_std/bonus/hashtable/hashtable.h"
+#include "rex_std/bonus/utility/use_self.h"
+#include "rex_std/bonus/hashtable/mod_range_hashing.h"
+#include "rex_std/internal/utility/pair.h"
+
+namespace rsl
+{
+  inline namespace v1
+  {
+		template <typename Value, typename Hash = rsl::hash<Value>, typename Predicate = rsl::equal_to<Value>,
+			typename Allocator = rsl::allocator>
+			class hash_set : public hashtable<Value, Value, Allocator, rsl::use_self<Value>, Predicate, Hash, mod_range_hashing, prime_rehash_policy, false, true>
+		{
+		public:
+			using base_type = hashtable<Value, Value, Allocator, rsl::use_self<Value>, Predicate, Hash, mod_range_hashing, prime_rehash_policy, false, true>;
+			using this_type = hash_set<Value, Hash, Predicate, Allocator>;
+			using size_type = typename base_type::size_type;
+			using value_type = typename base_type::value_type;
+			using allocator_type = typename base_type::allocator_type;
+			using node_type = typename base_type::node_type;
+
+		public:
+			/// hash_set
+			///
+			/// Default constructor.
+			/// 
+			explicit hash_set(const allocator_type& allocator = allocator_type())
+				: base_type(0_size, Hash(), mod_range_hashing(), Predicate(), rsl::use_self<Value>(), allocator)
+			{
+				// Empty
+			}
+
+
+			/// hash_set
+			///
+			/// Constructor which creates an empty container, but start with nBucketCount buckets.
+			/// We default to a small nBucketCount value, though the user really should manually 
+			/// specify an appropriate value in order to prevent memory from being reallocated.
+			///
+			explicit hash_set(size_type nBucketCount, const Hash& hashFunction = Hash(), const Predicate& predicate = Predicate(),
+				const allocator_type& allocator = allocator_type())
+				: base_type(nBucketCount, hashFunction, mod_range_hashing(), predicate, rsl::use_self<Value>(), allocator)
+			{
+				// Empty
+			}
+
+
+			hash_set(const this_type& x)
+				: base_type(x)
+			{
+			}
+
+
+			hash_set(this_type&& x)
+				: base_type(rsl::move(x))
+			{
+			}
+
+
+			hash_set(this_type&& x, const allocator_type& allocator)
+				: base_type(rsl::move(x), allocator)
+			{
+			}
+
+
+			/// hash_set
+			///
+			/// initializer_list-based constructor. 
+			/// Allows for initializing with brace values (e.g. hash_set<int> hs = { 3, 4, 5, }; )
+			///     
+			hash_set(std::initializer_list<value_type> ilist, size_type nBucketCount = 0, const Hash& hashFunction = Hash(),
+				const Predicate& predicate = Predicate(), const allocator_type& allocator = allocator_type())
+				: base_type(ilist.begin(), ilist.end(), nBucketCount, hashFunction, mod_range_hashing(), predicate, rsl::use_self<Value>(), allocator)
+			{
+				// Empty
+			}
+
+
+			/// hash_set
+			///
+			/// An input bucket count of <= 1 causes the bucket count to be equal to the number of 
+			/// elements in the input range.
+			///
+			template <typename FowardIterator>
+			hash_set(FowardIterator first, FowardIterator last, size_type nBucketCount = 0, const Hash& hashFunction = Hash(),
+				const Predicate& predicate = Predicate(), const allocator_type& allocator = allocator_type())
+				: base_type(first, last, nBucketCount, hashFunction, mod_range_hashing(), predicate, rsl::use_self<Value>(), allocator)
+			{
+				// Empty
+			}
+
+
+			this_type& operator=(const this_type& x)
+			{
+				return static_cast<this_type&>(base_type::operator=(x));
+			}
+
+
+			this_type& operator=(std::initializer_list<value_type> ilist)
+			{
+				return static_cast<this_type&>(base_type::operator=(ilist));
+			}
+
+
+			this_type& operator=(this_type&& x)
+			{
+				return static_cast<this_type&>(base_type::operator=(rsl::move(x)));
+			}
+
+		}; // hash_set
+
+		/// hash_set erase_if
+		///
+		/// https://en.cppreference.com/w/cpp/container/unordered_set/erase_if
+		template <typename Value, typename Hash, typename Predicate, typename Allocator, typename UserPredicate>
+		void erase_if(rsl::hash_set<Value, Hash, Predicate, Allocator>& c, UserPredicate predicate)
+		{
+			// Erases all elements that satisfy the predicate pred from the container.
+			for (auto i = c.begin(), last = c.end(); i != last;)
+			{
+				if (predicate(*i))
+				{
+					i = c.erase(i);
+				}
+				else
+				{
+					++i;
+				}
+			}
+		}
+
+
+		/// hash_multiset
+		///
+		/// Implements a hash_multiset, which is the same thing as a hash_set 
+		/// except that contained elements need not be unique. See the documentation 
+		/// for hash_set for details.
+		///
+		template <typename Value, typename Hash = rsl::hash<Value>, typename Predicate = rsl::equal_to<Value>,
+			typename Allocator = rsl::allocator>
+			class hash_multiset : public hashtable<Value, Value, Allocator, rsl::use_self<Value>, Predicate, Hash, mod_range_hashing, prime_rehash_policy, false, false>
+		{
+		public:
+			using base_type = hashtable<Value, Value, Allocator, rsl::use_self<Value>, Predicate, Hash, mod_range_hashing, prime_rehash_policy, false, false>;
+			using this_type = hash_multiset<Value, Hash, Predicate, Allocator>;
+			using size_type = typename base_type::size_type;
+			using value_type = typename base_type::value_type;
+			using allocator_type = typename base_type::allocator_type;
+			using node_type = typename base_type::node_type;
+
+		public:
+			/// hash_multiset
+			///
+			/// Default constructor.
+			/// 
+			explicit hash_multiset(const allocator_type& allocator = allocator_type())
+				: base_type(0, Hash(), mod_range_hashing(), Predicate(), rsl::use_self<Value>(), allocator)
+			{
+				// Empty
+			}
+
+
+			/// hash_multiset
+			///
+			/// Constructor which creates an empty container, but start with nBucketCount buckets.
+			/// We default to a small nBucketCount value, though the user really should manually 
+			/// specify an appropriate value in order to prevent memory from being reallocated.
+			///
+			explicit hash_multiset(size_type nBucketCount, const Hash& hashFunction = Hash(),
+				const Predicate& predicate = Predicate(), const allocator_type& allocator = allocator_type())
+				: base_type(nBucketCount, hashFunction, mod_range_hashing(), predicate, rsl::use_self<Value>(), allocator)
+			{
+				// Empty
+			}
+
+
+			hash_multiset(const this_type& x)
+				: base_type(x)
+			{
+			}
+
+
+			hash_multiset(this_type&& x)
+				: base_type(rsl::move(x))
+			{
+			}
+
+
+			hash_multiset(this_type&& x, const allocator_type& allocator)
+				: base_type(rsl::move(x), allocator)
+			{
+			}
+
+
+			/// hash_multiset
+			///
+			/// initializer_list-based constructor. 
+			/// Allows for initializing with brace values (e.g. hash_set<int> hs = { 3, 3, 4, }; )
+			///     
+			hash_multiset(std::initializer_list<value_type> ilist, size_type nBucketCount = 0, const Hash& hashFunction = Hash(),
+				const Predicate& predicate = Predicate(), const allocator_type& allocator = allocator_type())
+				: base_type(ilist.begin(), ilist.end(), nBucketCount, hashFunction, mod_range_hashing(), predicate, rsl::use_self<Value>(), allocator)
+			{
+				// Empty
+			}
+
+
+			/// hash_multiset
+			///
+			/// An input bucket count of <= 1 causes the bucket count to be equal to the number of 
+			/// elements in the input range.
+			///
+			template <typename FowardIterator>
+			hash_multiset(FowardIterator first, FowardIterator last, size_type nBucketCount = 0, const Hash& hashFunction = Hash(),
+				const Predicate& predicate = Predicate(), const allocator_type& allocator = allocator_type())
+				: base_type(first, last, nBucketCount, hashFunction, mod_range_hashing(), predicate, rsl::use_self<Value>(), allocator)
+			{
+				// Empty
+			}
+
+
+			this_type& operator=(const this_type& x)
+			{
+				return static_cast<this_type&>(base_type::operator=(x));
+			}
+
+
+			this_type& operator=(std::initializer_list<value_type> ilist)
+			{
+				return static_cast<this_type&>(base_type::operator=(ilist));
+			}
+
+
+			this_type& operator=(this_type&& x)
+			{
+				return static_cast<this_type&>(base_type::operator=(rsl::move(x)));
+			}
+
+		}; // hash_multiset
+
+		/// hash_multiset erase_if
+		///
+		/// https://en.cppreference.com/w/cpp/container/unordered_multiset/erase_if
+		template <typename Value, typename Hash, typename Predicate, typename Allocator, typename UserPredicate>
+		void erase_if(rsl::hash_multiset<Value, Hash, Predicate, Allocator>& c, UserPredicate predicate)
+		{
+			// Erases all elements that satisfy the predicate pred from the container.
+			for (auto i = c.begin(), last = c.end(); i != last;)
+			{
+				if (predicate(*i))
+				{
+					i = c.erase(i);
+				}
+				else
+				{
+					++i;
+				}
+			}
+		}
+
+
+
+		///////////////////////////////////////////////////////////////////////
+		// global operators
+		///////////////////////////////////////////////////////////////////////
+
+		template <typename Value, typename Hash, typename Predicate, typename Allocator>
+		inline bool operator==(const hash_set<Value, Hash, Predicate, Allocator>& a,
+			const hash_set<Value, Hash, Predicate, Allocator>& b)
+		{
+			using const_iterator = typename hash_set<Value, Hash, Predicate, Allocator>::const_iterator ;
+
+			// We implement branching with the assumption that the return value is usually false.
+			if (a.size() != b.size())
+				return false;
+
+			// For set (with its unique keys), we need only test that each element in a can be found in b,
+			// as there can be only one such pairing per element. multiset needs to do a something more elaborate.
+			for (const_iterator ai = a.begin(), aiEnd = a.end(), biEnd = b.end(); ai != aiEnd; ++ai)
+			{
+				const_iterator bi = b.find(*ai);
+
+				if ((bi == biEnd) || !(*ai == *bi)) // We have to compare values in addition to making sure the lookups succeeded. This is because the lookup is done via the user-supplised Predicate
+					return false;                  // which isn't strictly required to be identical to the Value operator==, though 99% of the time it will be so.  
+			}
+
+			return true;
+		}
+
+		template <typename Value, typename Hash, typename Predicate, typename Allocator>
+		inline bool operator!=(const hash_set<Value, Hash, Predicate, Allocator>& a,
+			const hash_set<Value, Hash, Predicate, Allocator>& b)
+		{
+			return !(a == b);
+		}
+
+
+		template <typename Value, typename Hash, typename Predicate, typename Allocator>
+		inline bool operator==(const hash_multiset<Value, Hash, Predicate, Allocator>& a,
+			const hash_multiset<Value, Hash, Predicate, Allocator>& b)
+		{
+			using const_iterator = typename hash_multiset<Value, Hash, Predicate, Allocator>::const_iterator;
+			typedef typename rsl::iterator_traits<const_iterator>::difference_type difference_type;
+
+			// We implement branching with the assumption that the return value is usually false.
+			if (a.size() != b.size())
+				return false;
+
+			// We can't simply search for each element of a in b, as it may be that the bucket for 
+			// two elements in a has those same two elements in b but in different order (which should 
+			// still result in equality). Also it's possible that one bucket in a has two elements which 
+			// both match a solitary element in the equivalent bucket in b (which shouldn't result in equality).
+			rsl::pair<const_iterator, const_iterator> aRange;
+			rsl::pair<const_iterator, const_iterator> bRange;
+
+			for (const_iterator ai = a.begin(), aiEnd = a.end(); ai != aiEnd; ai = aRange.second) // For each element in a...
+			{
+				aRange = a.equal_range(*ai); // Get the range of elements in a that are equal to ai.
+				bRange = b.equal_range(*ai); // Get the range of elements in b that are equal to ai.
+
+				// We need to verify that aRange == bRange. First make sure the range sizes are equivalent...
+				const difference_type aDistance = rsl::distance(aRange.first, aRange.second);
+				const difference_type bDistance = rsl::distance(bRange.first, bRange.second);
+
+				if (aDistance != bDistance)
+					return false;
+
+				// At this point, aDistance > 0 and aDistance == bDistance.
+				// Implement a fast pathway for the case that there's just a single element.
+				if (aDistance == 1)
+				{
+					if (!(*aRange.first == *bRange.first))   // We have to compare values in addition to making sure the distance (element count) was equal. This is because the lookup is done via the user-supplised Predicate
+						return false;                       // which isn't strictly required to be identical to the Value operator==, though 99% of the time it will be so. Ditto for the is_permutation usage below.
+				}
+				else
+				{
+					// Check to see if these aRange and bRange are any permutation of each other. 
+					// This check gets slower as there are more elements in the range.
+					if (!rsl::is_permutation(aRange.first, aRange.second, bRange.first))
+						return false;
+				}
+			}
+
+			return true;
+		}
+
+		template <typename Value, typename Hash, typename Predicate, typename Allocator>
+		inline bool operator!=(const hash_multiset<Value, Hash, Predicate, Allocator>& a,
+			const hash_multiset<Value, Hash, Predicate, Allocator>& b)
+		{
+			return !(a == b);
+		}
+
+  }
+}
--- source/rex_std/include/rex_std/unordered_set.h
+++ source/rex_std/include/rex_std/unordered_set.h
@@ -12,48 +12,18 @@
 
 #pragma once
 
-#include "rex_std/bonus/defines.h"
-#include "rex_std/bonus/memory/typed_allocator.h"
-#include "rex_std/compare.h"
-#include "rex_std/disable_std_checking.h"
-#include "rex_std/initializer_list.h"
-#include "rex_std/std_alias_defines.h"
-
-#include <unordered_set>
+#include "rex_std/bonus/hashtable/hash_set.h"
+#include "rex_std/internal/functional/equal_to.h"
 
 namespace rsl
 {
   inline namespace v1
   {
+    template <typename Value, typename Hash = rsl::hash<Value>, typename Predicate = rsl::equal_to<Value>, typename Allocator = rsl::allocator>
+    using unordered_set = hash_set<Value, Hash, Predicate, Allocator>;
 
-    template <typename T>
-    struct hash;
-
-    template <typename T>
-    struct equal_to;
-
-    class allocator;
-
-    REX_STD_TEMPLATED_CLASS_ALIAS(template <typename Key, typename Hash = hash<Key>, typename KeyEqual = equal_to<Key>, typename Allocator = typed_allocator<Key>>, unordered_set, Key, Hash, KeyEqual, Allocator);
-
-    REX_STD_TEMPLATED_CLASS_ALIAS(template <typename Key, typename Hash = hash<Key>, typename KeyEqual = equal_to<Key>, typename Allocator = typed_allocator<Key>>, unordered_multiset, Key, Hash, KeyEqual, Allocator);
-
-    REX_STD_FUNC_ALIAS(operator==);
-    REX_STD_FUNC_ALIAS(operator!=);
-
-    REX_STD_FUNC_ALIAS(erase_if);
-
-    REX_STD_FUNC_ALIAS(begin);
-    REX_STD_FUNC_ALIAS(cbegin);
-    REX_STD_FUNC_ALIAS(cend);
-    REX_STD_FUNC_ALIAS(rbegin);
-    REX_STD_FUNC_ALIAS(crbegin);
-    REX_STD_FUNC_ALIAS(rend);
-    REX_STD_FUNC_ALIAS(crend);
-    REX_STD_FUNC_ALIAS(size);
-    REX_STD_FUNC_ALIAS(ssize);
-    REX_STD_FUNC_ALIAS(empty);
-    REX_STD_FUNC_ALIAS(data);
+    template <typename Value, typename Hash = rsl::hash<Value>, typename Predicate = rsl::equal_to<Value>, typename Allocator = rsl::allocator>
+    using unordered_multiset = hash_multiset<Value, Hash, Predicate, Allocator>;
 
   } // namespace v1
 } // namespace rsl
