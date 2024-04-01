a2df56c9da9ef599b43890c0ef4f5b8a01d42f48
Nick De Breuck <nick.debreuck@outlook.com>
2024-04-01 19:04:32 +0100

    [main] adding some test code for iterators

--- tests/rex_std_test/src/test_iterator.cpp
+++ tests/rex_std_test/src/test_iterator.cpp
@@ -10,10 +10,38 @@
 //
 // ============================================
 
-namespace rsl::test
-{
-  inline namespace v1
-  {
+#include "rex_std_test/catch2/catch.hpp"
 
-  }
+#include "rex_std/iterator.h"
+#include "rex_std/array.h"
+
+TEST_CASE("Iterator Advance")
+{
+  rsl::array arr = { 1,2,3 };
+  auto it = arr.cbegin();
+
+  rsl::advance(it, 1);
+
+  CHECK(it == arr.cbegin() + 1);
+  CHECK(*it == 2);
+
+  rsl::advance(it, -1);
+
+  CHECK(it == arr.cbegin());
+  CHECK(*it == 1);
+
+  rsl::advance(it, 2);
+
+  CHECK(it == arr.cbegin() + 2);
+  CHECK(*it == 3);
+}
+
+TEST_CASE("Move Iterator")
+{
+
+}
+
+TEST_CASE("Reverse Iterator")
+{
+
 }
