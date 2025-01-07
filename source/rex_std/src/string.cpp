c24c893b3ca4cb13b1d8547642ce4025577b5630
Nick De Breuck <nick.debreuck@outlook.com>
2025-01-07 00:07:15 +0000

    [main] optimization to the split functions

--- source/rex_std/src/string.cpp
+++ source/rex_std/src/string.cpp
@@ -1,5 +1,7 @@
 #include "rex_std/bonus/string.h"
 
+#include "rex_std/algorithm.h"
+
 namespace rsl
 {
   inline namespace v1
@@ -13,6 +15,16 @@ namespace rsl
         using string_view_type = rsl::basic_string_view<CharType, Traits>;
 
         rsl::vector<string_view_type> result;
+
+        // Quick memory optimization to avoid reallocation later on
+        // Always starts with 1 as in worst case we need to copy the entire string
+        card32 delim_in_str = 1;
+        for (CharType c : deliminators)
+        {
+          delim_in_str += rsl::count(str.cbegin(), str.end(), c);
+        }
+        result.reserve(delim_in_str);
+
         card32 start = 0;
         for (card32 i = 0; i < str.size(); ++i)
         {
